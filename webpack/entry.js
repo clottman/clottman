ac7e1b2d03421b997161fa622e1d41808b580824
Cassey Lottman <clottman@live.com>
2018-03-12 21:01:02 -0400

    add search

--- webpack/entry.js
+++ webpack/entry.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import {render} from 'react-dom';
 import Hello from './components/Hello';
 
+import Search from './search.js';
+let search = new Search();
+
 class App extends Component {
   render() {
     return (
